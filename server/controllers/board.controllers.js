f53f94ac992ae9d099fd1828d5c0023d4c20fd26
Elena Mederos <elenamc91@hotmail.com>
2022-12-14 22:24:36 +0100

    endspoints básicos del boards controller

--- server/controllers/board.controllers.js
+++ server/controllers/board.controllers.js
@@ -1,55 +1,102 @@
-const sequelize = require("../dataBases/mysql");
+const conexion = require("../dataBases/mysql");
 const boardsModel = require("../models/boards.model");
+// const userboard = require ("./user_boards.controllers");
+const user = require ("./user.controllers");
 
 const board = {
   getAll: async (req, res) => {
-    const boards = await boardsModel.findAll();
-    res.json(boards);
-  },
-
-  getByWork: async (req, res) => {
-    const boards = await boardsModel.findAll();
-    res.json(boards);
+    try {
+      var con = await conexion.abrir();
+      const boardM = await boardsModel.create(con);
+      const boards = await boardM.findAll();
+      res.json(boards);
+    } catch (e) {
+      console.log(e);
+      res.json(false);
+    }
+    finally {
+      await conexion.cerrar(con);
+    }
   },
 
   insert: async (req, res) => {
     try{
-        const { id, name_, visibility, configuration } = req.body;
-        const existboard = await boardsModel.findAll({ where: { id } });
-        if (existboard == "") {
-            await boardsModel.update({ id, name_, visibility, configuration },{ where: { id } });
+        const { id, name_, visibility, configuration, fk_id_workspace, fk_id_user } = req.body;
+        var con = await conexion.abrir();
+        const boardM = await boardsModel.create(con);
+        const board = await boardM.findOne({ where: { name_ } });
+        if (!board) {
+            await boardM.create({ id, name_, visibility, configuration, fk_id_workspace, fk_id_user });
             res.json(true);
         }else{
-            res.json({msn:" ya existe"});
+            res.json({msn:"Existe con ese nombre"});
         }
-        
     }catch(e){
         console.log(e);
         res.json(false);
+    }finally {
+      await conexion.cerrar(con);
+    }
+  },
+
+  show: async (req, res) => {
+    try{
+        var con = await conexion.abrir();
+        const boardM = await boardsModel.create(con);
+        const board = await boardM.findOne({ where: { id:req.params.id } });
+        res.json(board)
+    }catch(e){
+        console.log(e);
+        res.json(false);
+    }finally {
+      await conexion.cerrar(con);
     }
   },
 
   update: async (req, res) => {
     try{
         const { id, name_, visibility, configuration } = req.body;
-        await boardsModel.update({ id, name_, visibility, configuration },{ where: { id } });
-        res.json(true);
+        var con = await conexion.abrir();
+        const boardM = await boardsModel.create(con);
+        const board = await boardM.findOne({ where: { id } });
+        if (board) {
+            await boardM.update({ name_, visibility, configuration },{ where: { id } });
+            res.json(true);
+        }else{
+            res.json({msn:"no existe"});
+        }
     }catch(e){
         console.log(e);
         res.json(false);
+    }finally {
+      await conexion.cerrar(con);
     }
   },
 
   delete: async (req, res) => {
     try{
         const { id } = req.body;
-        await boardsModel.destroy({ where: { id } });
+        var con = await conexion.abrir();
+        const boardM = await boardsModel.create(con);
+        await boardM.destroy({ where: { id } });
         res.json(true);
     }catch(e){
         console.log(e);
         res.json(false);
+    }finally {
+      await conexion.cerrar(con);
     }
   },
+
+  // getByUser:async (req, res) => {
+  //   try{
+  //       const boards = await userboard.getboardsByUser(user.getId());
+  //       res.json(boards);
+  //   }catch(e){
+  //       console.log(e);
+  //       res.json(false);
+  //   }
+  // }
 };
 
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,6 +3,7 @@ const activities = require("../controllers/activities.controllers");
 const message = require("../controllers/messages.controllers");
 const user = require("../controllers/user.controllers");
 const workspace = require("../controllers/workspace.controllers");
+const board = require("../controllers/board.controllers");
 
 //WORKSPACE
 router.get("/all-workspaces", workspace.getAll);
@@ -11,6 +12,14 @@ router.get("/show-workspace/:id", workspace.show);
 router.post("/update-workspace", workspace.update);
 router.delete("/delete-workspace", workspace.delete);
 // router.get("get-workspaces-by-user", workspace.getByUser);
+
+//BOARDS
+router.get("/all-boards", board.getAll);
+router.post("/insert-board", board.insert);
+router.get("/show-board/:id", board.show);
+router.post("/update-board", board.update);
+router.delete("/delete-board", board.delete);
+// router.get("get-workspaces-by-user", board.getByUser);
 
 //MESSAGES
 router.post("/insertmessage", message.insert);
