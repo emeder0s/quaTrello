bd00c8e8060f44ae1e6dda5b5479b3a87e995e82
Elena Mederos <elenamc91@hotmail.com>
2022-12-14 17:44:30 +0100

    Corregido el workspace controller

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,7 +5,7 @@ const workspace = require("../controllers/workspace.controllers");
 //WORKSPACE
 router.get("/all-workspaces", workspace.getAll);
 router.post("/insert-workspace", workspace.insert);
-router.get("/show-workspace", workspace.show);
+router.get("/show-workspace/:id", workspace.show);
 router.post("/update-workspace", workspace.update);
 router.delete("/delete-workspace", workspace.delete);
 // router.get("get-workspaces-by-user", workspace.getByUser);
