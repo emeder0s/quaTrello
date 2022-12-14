6403f3a330bb85bd5178bf2d6b165e8c2054b34e
mariadelaverde98 <mariadelaverde98@gmail.com>
2022-12-14 17:30:52 +0100

    merge

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const activities = require("../controllers/activities.controllers");
 const message = require("../controllers/messages.controllers");
 const workspace = require("../controllers/workspace.controllers");
 
@@ -10,9 +11,14 @@ router.post("/update-workspace", workspace.update);
 router.delete("/delete-workspace", workspace.delete);
 // router.get("get-workspaces-by-user", workspace.getByUser);
 
-
 //MESSAGES
 router.post("/insertmessage", message.insert);
 router.post("/getmessages", message.getMsgs);
+
+//ACTIVITIES
+router.post("/getactivities", activities.get);
+router.post("/insertactivities", activities.insert);
+router.post("/updateactivities", activities.update);
+router.post("/deleteactivities", activities.delete);
 
 module.exports = router;
