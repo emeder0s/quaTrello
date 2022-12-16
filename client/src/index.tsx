6874b5a3fc3e9d4e8b5d559dad9a57972677ddc9
Henalu <henaludebarros@hotmail.com>
2022-12-16 11:25:17 +0100

    board y login

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 import { Rutas } from './router/Rutas';
 import "./sass/styles.scss"
+import { Header } from './components/layouts/Header'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
- <div className='index'>
+  <div className='index'>
+    <Header />
     <Rutas />
-    </div>
+  </div>
 );
