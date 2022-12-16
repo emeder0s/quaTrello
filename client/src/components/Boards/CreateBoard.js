4cb82759e210fe908705f5211d5c0d9ae4b9cf65
gmirg <gofthet@gmail.com>
2022-12-16 16:03:51 +0100

    fetch de imagenes solo funciona una vez

--- client/src/components/Boards/CreateBoard.js
+++ client/src/components/Boards/CreateBoard.js
@@ -1,8 +1,34 @@
 import React, { useEffect, useState } from "react";
+import { json } from "react-router-dom";
 import boardPreview from '../../img/board-preview.svg';
 
-export const CreateBoard = (props) => {
 
+export const CreateBoard = () => {  
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        let url = "https://api.unsplash.com/photos/random?client_id=JMjfYjVegEZWmnba38YxaDVZaPQx4fH75Dnxq3mUN1E&count=4"
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                const json = await response.json();
+                setData(json);
+                console.log(data)
+                } catch (error) {
+                console.log("error", error);
+                }
+        };
+        fetchData();
+    }, []);
+
+    // const getBackgrounds = async () => {
+    //     await fetch("https://api.unsplash.com/photos/random?client_id=JMjfYjVegEZWmnba38YxaDVZaPQx4fH75Dnxq3mUN1E&count=4")
+    //         .then(res => res.json())
+    //         .then(json => {let datos = json})
+    //     console.log(data)
+    // }
+    // useEffect(() => {
+    //     getBackgrounds();
+    // }, [getBackgrounds])
     return (
         <section className="create-board">
             <header>
@@ -12,15 +38,15 @@ export const CreateBoard = (props) => {
             <hr></hr>
             <div className="chooseBG">
                 <div className="board-bg"><img src={boardPreview}></img></div>
-                
+
             </div>
             <h4>Background</h4>
             <form >
                 <ul className="images" >
-                    <li><button className="image #1"></button></li>
-                    <li><button className="image #2"></button></li>
-                    <li><button className="image #3"></button></li>
-                    <li><button className="image #4"></button></li>
+                    <li><button className="image #1" style={{ backgroundImage: `url(${data[0].urls.thumb})` }}></button></li>
+                    <li><button className="image #2" style={{ backgroundImage: `url(${data[1].urls.thumb})` }}></button></li>
+                    <li><button className="image #3" style={{ backgroundImage: `url(${data[2].urls.thumb})` }}></button></li>
+                    <li><button className="image #4" style={{ backgroundImage: `url(${data[3].urls.thumb})` }}></button></li>
                 </ul>
                 <ul className="colors">
                     <li><button className="bg c1"></button></li>
@@ -45,7 +71,7 @@ export const CreateBoard = (props) => {
                     <option>Public</option>
                 </select>
                 <div>
-                <button className="submit" type="submit">Create</button>
+                    <button className="submit" type="submit">Create</button>
                 </div>
             </form>
         </section>
