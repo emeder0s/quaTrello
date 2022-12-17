3d5efc1950eac177ec0a6a018d0e222cfa9c237b
JorgeCoronilla <jorge.coronilla.naranjo@gmail.com>
2022-12-17 12:15:40 +0100

    Listas y tarjetas

---
+++ client/src/components/Board/ContentBoard/AddList.js
@@ -0,0 +1,35 @@
+import React, { useState, useContext } from 'react'
+import { BoardContext } from '../../../providers/boardProvider';
+
+export const AddList = () => {
+  const {setNewListTitle} = useContext(BoardContext);
+  const [showInput, setShowInput] = useState(false);
+  const add = () => {
+    setShowInput(!showInput);
+  }
+
+  const addTitle = e => {
+    e.preventDefault();
+      
+    let title = {
+      title: e.target.title.value,
+      cards: "none"}
+      setNewListTitle(title)
+  }
+
+  if (showInput) {
+    return (
+      <div className='listInput'>
+        <form onSubmit={addTitle}>
+        <input type="text" placeholder="Introduzca el titulo de la lista..." required name="title" />
+        <button type="submit" className='addList'>Añadir lista </button>
+        <button className='close' onClick={add}>&#x2715;</button>
+        </form>
+      </div>
+    )
+  }
+  return (
+    <div className='addlistStart' onClick={add}>
+      &#10010; AddList</div>
+  )
+}
