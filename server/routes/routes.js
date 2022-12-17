00a5dc046bb397976aa37c015adced4774b2333e
Elena Mederos <elenamc91@hotmail.com>
2022-12-17 11:21:45 +0100

    Añadido endpoint de modificación de last_access de un workspace

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -15,6 +15,7 @@ router.get("/show-workspace/:id", workspace.show);
 router.post("/update-workspace", workspace.update);
 router.delete("/delete-workspace", workspace.delete);
 router.get("/get-workspaces-by-user", workspace.getByUser);
+router.post("/update-last-access", workspace.updateLastAccess);
 
 //BOARDS
 router.get("/all-boards", board.getAll);
@@ -45,7 +46,6 @@ router.post("/getactivities", activities.get);
 router.post("/insertactivities", activities.insert);
 router.post("/updateactivities", activities.update);
 router.post("/deleteactivities", activities.delete);
-
 
 //USER
 router.post("/confirmEmail",user.confirmEmail);
