170a33da211213590dbd0f54a3292de2d2d735eb
Henalu <henaludebarros@hotmail.com>
2022-12-19 10:24:33 +0100

    helper corregido

--- client/src/helpers/defaultFetch.js
+++ client/src/helpers/defaultFetch.js
@@ -1,31 +1,29 @@
 export const defaultFetch = async (endPoint, metodo, datos) => {
-    let metaData = {
-        method: metodo,
-        body: JSON.stringify(datos),
-        mode: "cors",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-type": "application/json",
-        },
-      };
+  let metaData = {
+    method: metodo,
+    body: JSON.stringify(datos),
+    mode: "cors",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-type": "application/json",
+    },
+  };
 
-      const res = (await fetch(endPoint, metaData)).json();
-        return res;
+  const res = (await fetch(endPoint, metaData)).json();
+  return res;
 }
 
-
 export const getFetch = async (endPoint, metodo, datos) => {
-    let metaData = {
-        method: metodo,
-        body: JSON.stringify(datos),
-        mode: "cors",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-type": "application/json",
-        },
-      };
+  let metaData = {
+    method: metodo,
+    body: JSON.stringify(datos),
+    mode: "cors",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-type": "application/json",
+    },
+  };
   console.log(metaData)
-      await fetch(endPoint, metaData)
-      .then((res) => res.json())
-      .then(res=>{return res})
+  const res = (await fetch(endPoint, metaData)).json();
+  return res;
 }
