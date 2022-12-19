48528ea8dd10ecb5554059d0a0aa9ae0b8b78c31
Henalu <henaludebarros@hotmail.com>
2022-12-19 12:35:22 +0100

    limpiando comentarios

--- client/src/components/Home/SideMenu.js
+++ client/src/components/Home/SideMenu.js
@@ -4,9 +4,6 @@ import { useSelector } from 'react-redux'
 
 export const SideMenu = ({ setIsFormOpen }) => {
 
-  // Crear una funcion que busque espacios de trabajo. Si existen se cambia el estado a true y se muestran los que sean en el listado
-  // const [isWorkSpaceCreated, setIsWorkSpaceCreated] = useState(false)
-
   const reduxWorkspaces = useSelector(state => state.workspaces.workspaces)
 
   return (
