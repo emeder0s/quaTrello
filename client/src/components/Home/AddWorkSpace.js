c42e7bb0d1a6b5db40a5869aac7742ff1d69aecb
Henalu <henaludebarros@hotmail.com>
2022-12-19 16:38:55 +0100

    post de workspaces

--- client/src/components/Home/AddWorkSpace.js
+++ client/src/components/Home/AddWorkSpace.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from 'react'
 import { AiOutlineClose } from 'react-icons/ai'
 import formIMG from '../../img/formIMG.svg'
 import { defaultEnableSubmit } from '../../helpers/disableButton'
+import { defaultFetch } from '../../helpers/defaultFetch'
+import { Navigate } from 'react-router-dom'
 
 const AddWorkSpace = ({ setIsFormOpen }) => {
 
     // const [isInputDisabled, setIsInputDisabled] = useState('disabled')
-
+    const [navigate, setNavigate] = useState(false)
+    const [board, setBoard] = useState('')
     const [formValues, setFormValues] = useState({
         name: "",
         type: "",
+        visibility: '',
+        configuration: {},
         description: ""
     })
 
@@ -17,9 +22,9 @@ const AddWorkSpace = ({ setIsFormOpen }) => {
     defaultEnableSubmit(isButtonEnabled, 'input[type="submit"]')
 
     useEffect(() => {
-        if (formValues.name === '' || formValues.type === '' || formValues.type === 'Elegir...') {
+        if (formValues.name === '' || formValues.type === '' || formValues.type === 'Elegir...' || formValues.visibility === '' || formValues.visibility === 'Elegir...') {
             setIsButtonEnabled(false)
-        }else if(formValues.name !== '' && formValues.type !== ''){
+        } else {
             setIsButtonEnabled(true)
         }
     }, [formValues])
@@ -32,6 +37,10 @@ const AddWorkSpace = ({ setIsFormOpen }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        console.log(formValues)
+        setBoard('/board/1')
+        setNavigate(true)
+        defaultFetch('/insert-workspace', 'POST', formValues)
     }
 
     return (
@@ -43,6 +52,7 @@ const AddWorkSpace = ({ setIsFormOpen }) => {
                         <h4>Impulse su productividad facilitándoles a todos el acceso a los tableros en una única ubicación.</h4>
 
                         <label htmlFor='workspaceName'>Nombre del Espacio de trabajo</label>
+
                         <input
                             type='text'
                             placeholder='your workspace'
@@ -50,7 +60,6 @@ const AddWorkSpace = ({ setIsFormOpen }) => {
                             value={formValues.name}
                             onChange={handleChange}
                         />
-                        <p>Este es el nombre de su empresa, equipo u organización.</p>
 
                         <label htmlFor='type'>Tipo de Espacio de trabajo</label>
                         <select name='type' value={formValues.type} onChange={handleChange}>
@@ -65,9 +74,20 @@ const AddWorkSpace = ({ setIsFormOpen }) => {
                             <option>Otros</option>
                         </select>
 
-                        <label htmlFor='description'>Descripción del Espacio de trabajo <span>Opcional</span></label>
-                        <textarea name='description' value={formValues.description} onChange={handleChange}></textarea>
-                        <p>Incorpora a los miembros con unas cuantas palabras sobre tu Espacio de trabajo.</p>
+                        <label htmlFor='visibility'>Visibilidad del Espacio de trabajo</label>
+                        <select name='visibility' value={formValues.visibility} onChange={handleChange}>
+                            <option default>Elegir...</option>
+                            <option>Public</option>
+                            <option>Private</option>
+                        </select>
+
+                        <label htmlFor='description'>Descripción del Espacio de trabajo <span>(Opcional)</span></label>
+                        <textarea
+                            name='description'
+                            placeholder='Envía a los miembros de tu equipo unas cuantas palabras sobre el Espacio de trabajo.'
+                            value={formValues.description}
+                            onChange={handleChange}>
+                        </textarea>
                         <input disabled type='submit' value='Continuar' />
                     </form>
                 </div>
@@ -76,6 +96,9 @@ const AddWorkSpace = ({ setIsFormOpen }) => {
                     <img src={formIMG} alt='icono' />
                 </div>
             </div>
+            {navigate && (
+                <Navigate to={board} replace={true} />
+            )}
         </div>
     )
 }
