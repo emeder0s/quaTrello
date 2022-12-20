570b1711406514d43269540cc4a65276ad64ebe5
SERGIOOVEP <sergio.ovejeropascual@gmail.com>
2022-12-20 15:35:04 +0100

    nav Terminado

--- client/src/components/Board/MenuBoard/ShareNavBoard.js
+++ client/src/components/Board/MenuBoard/ShareNavBoard.js
@@ -1,19 +1,37 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 
-const modalContent = <form className="formModal">
-    <span>Compartir Tablero</span>
-    <input type="email" placeholder="Dirección de correo electrónico o nombre"/>
-</form>
+export const ShareNavBoard = ({ setIsModalShareOpen }) => {
+    
 
-/*
-hacer el fetch para que salgan todos los usuarios que están incluidos en ese tablero
-*/
-
+    const modalContent = <form className="formModal">
+        <span>Compartir Tablero</span>
+        <input type="email" default/>
+    </form>
 
 
+    let token = window.location.pathname.split("/")[2];
 
-export const ShareNavBoard = ({ showWindow, setIsModalShareOpen }) => {
+    const [shareMember, setShareMember] = useState([])
+
+    useEffect(() => {
+
+        const data = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ fk_id_board: token }),
+        };
+
+        fetch("/getUsersBoard", data)
+            .then((res) => res.json())
+            .then((res) => {
+                setShareMember(res);
+            });
+    }, []);
+
+
+
 
     return (
 
@@ -22,7 +40,22 @@ export const ShareNavBoard = ({ showWindow, setIsModalShareOpen }) => {
             <div >
                 <button onClick={e => setIsModalShareOpen(false)} className="butModal">x</button>
                 <label htmlFor="btn-modal" onClick={(e) => setIsModalShareOpen(true)}><div className=".modal">{modalContent}</div></label>
+                <br />
+                <br />
+                <br />
                 {setIsModalShareOpen}
+                {shareMember ? shareMember.map((member, i) => {
+                    return (
+                        <tr key={i} className="trSpaces">
+                            <td className='tdSpaces'>{member.full_name}</td>
+                            <br />
+                            <td className='tdSpaces'>{member.email}</td>
+                            <br />
+                        </tr>
+                    )
+                })
+                    : <p>"No hay miembros en el tablero"</p>
+                }
 
             </div>
         </div>
