4a5a490e3f85b440fb61b6e084c581ac327a7264
Henalu <henaludebarros@hotmail.com>
2022-12-20 11:58:45 +0100

    problemas de front resueltos

--- client/src/features/users/userIds.js
+++ client/src/features/users/userIds.js
@@ -2,18 +2,26 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = []
 
+function filterArray(array, filtro) {
+    const filtered = array.filter(element => element !== filtro)
+    return filtered
+}
+
 export const userIdsSlice = createSlice({
     name: 'userIds',
     initialState,
     reducers: {
         addUserID: (state, action) => {
-            console.log(state, action)
             state.push(action.payload)
-        }
+        },
+        deleteUserID: (state, action) => {
+            console.log(state, action)
+            state = filterArray(state, action.payload)
+        },
     }
 })
 
-export const { addUserID } = userIdsSlice.actions
+export const { addUserID, deleteUserID } = userIdsSlice.actions
 
 // Exportamos por defecto el reducer que es lo que quiere luego el store
 export default userIdsSlice.reducer
